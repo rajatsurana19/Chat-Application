5ede84963b590b3ef80692566c376feb55866a77
Rajat Surana <rasurana19@gmail.com>
2026-01-31 21:27:58 +0530

    Updated generate code function

--- main.py
+++ main.py
@@ -9,8 +9,38 @@ app.config["SECRET_KEY"] = "rajatschatapp"
 
 socektio = SocketIO(app)
 
+rooms = {}
+
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+
+        if code not in rooms:
+            break
+
+    return code
+
 @app.route("/", methods=["POST", "GET"])
 def home():
+    if request.method == 'POST':
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join",False)
+        create = request.form.get("create",False)
+
+        if not name:
+            return render_template("home.html",error="Please Enter a name")
+        if join != False and not code:
+            return render_template("home.html",error="Please enter a room code")
+        
+        room = code 
+        if create != False:
+            room = generate_unique_code(4)
+
+
+
     return render_template("home.html")
 
 if __name__ == "__main__":
